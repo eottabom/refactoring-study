e617fb22b0d7d9139d5fb6186f3f4fce91279f6f
eottabom <snoopy12oyk@gmail.com>
2025-05-26 12:20:23 +0900

    fix: 카탈로그 4. 데이터 조직화.

--- src/main/java/io/github/eottabom/refactoring/_00_catalog/catalog_04/package-info.java
+++ src/main/java/io/github/eottabom/refactoring/_00_catalog/catalog_04/package-info.java
@@ -5,6 +5,8 @@
  * Split Variable (변수 쪼개기)
  * Rename Field (필드 이름 바꾸기)
  * Replace Derived Variable with Query (파생 변수를 질의 함수로 바꾸기)
+ * - 계산할 수 있는 변수를 굳이 필드로 선언하지 말자는 것.
+ * - 다른 변수들을 통해서 계산할 수 있는 것은 함수로 바꾸자는 것.
  * Change References to Value (참조를 값으로 바꾸기)
  * Change Value to References (값을 참조로 바꾸기)
  */
