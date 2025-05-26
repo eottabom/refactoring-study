bee332cff975ce622832e6c2b479459955c7e197
eottabom <snoopy12oyk@gmail.com>
2025-05-26 13:03:25 +0900

    feat: 카탈로그 7. 상속 다루기.

---
+++ src/main/java/io/github/eottabom/refactoring/_00_catalog/catalog_07/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * 카탈로그 7. 상속 다루기.
+ * 상속을 제대로 사용하는 기술
+ *
+ * Pull up Method (메서드 올리기)
+ * Pull up Field (필드 올리기)
+ * Pull up Constructor Body (생성자 본문 올리기)
+ * Push Down Method (메서드 내리기)
+ * Push Down Field (필드 내리기)
+ * Replace Type Code with Subclasses (타입 코드를 서브 클래스로 바꾸기)
+ * Remove Subclass (서브클래스 제거하기)
+ * Extract Superclass (슈퍼 클래스 추출하기)
+ * Collapse Hierarchy (계층 합치기)
+ * Replace Subclass with Delegate (서브 클래스를 위임으로 바꾸기)
+ * Replace Superclass with Delegate (슈퍼 클래스를 위임으로 바꾸기)
+ */
+package io.github.eottabom.refactoring._00_catalog.catalog_07;
