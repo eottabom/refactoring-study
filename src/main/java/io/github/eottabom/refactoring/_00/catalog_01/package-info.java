8513fb54e8edfb123a3f3bdd373e4ab7afb1330c
eottabom <snoopy12oyk@gmail.com>
2025-05-26 12:07:08 +0900

    feat: 카탈로그 2. 캡슐화.

--- src/main/java/io/github/eottabom/refactoring/_00/catalog_01/package-info.java
+++ src/main/java/io/github/eottabom/refactoring/_00/catalog_01/package-info.java
@@ -1,1 +1,18 @@
+/**
+ * 카탈로그 2. 캡슐화.
+ * 모듈에서 외부 시스템으로 공개하지 않아도 되는 데이터를 숨기는 기술.
+ *
+ * Encapsulate Record (레코드 캡슐화하기)
+ * Encapsulate Collection (컬렉션 캡슐화하기)
+ * - 어떠한 클래스가 가지고 있는 컬렉션을 캡슐화하는 방법이다.
+ * - 컬렉션 자체를 캡슐화하는 것.
+ * - 컬렉션의 복사본을 전달하거나 컬렉션 자체를 제공하는게 아니라, 컬렉션을 수정할 수 있는 메서드를 제공한다거나..
+ * Replace Primitive with Object (기본형을 객체로 바꾸기)
+ * Replace Temp with Object (임시 변수를 질의 함수로 바꾸기)
+ * Extract Class (클래스 추출하기)
+ * Inline Class (클래스 인라인하기)
+ * Hide Delegate (위임 숨기기) <-> Remove Middle Man (중재자 제거하기)
+ * Remove Middle Man (중재자 제거하기) <-> Hide Delegate (위임 숨기기)
+ * Substitute Algorithm (알고리듬 교체하기)
+ */
 package io.github.eottabom.refactoring._00.catalog_01;
