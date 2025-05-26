f472bf886e70ff6e1a8cf2f33668cb0cb049b570
eottabom <snoopy12oyk@gmail.com>
2025-05-26 12:18:33 +0900

    feat: 카탈로그 4. 데이터 조직화.

---
+++ src/main/java/io/github/eottabom/refactoring/_00_catalog/catalog_04/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 카탈로그 4. 데이터 조직화.
+ * 데이터(변수가 필드) 구조를 다루는 기술
+ *
+ * Split Variable (변수 쪼개기)
+ * Rename Field (필드 이름 바꾸기)
+ * Replace Derived Variable with Query (파생 변수를 질의 함수로 바꾸기)
+ * Change References to Value (참조를 값으로 바꾸기)
+ * Change Value to References (값을 참조로 바꾸기)
+ */
+package io.github.eottabom.refactoring._00_catalog.catalog_04;
